7a9b5666232ddfe8374cea243b75c9d039395fc5
helio <heliojmsemedo@gmail.com>
2020-04-23 22:25:46 +0100

    Updates

---
+++ precache-manifest.bc783b4c66955d3deb38e0f3ec5b7644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d05d63b884c24bfcc9bb7cfbf5687f",
+    "url": "/moviedb_frontend/index.html"
+  },
+  {
+    "revision": "8e4d727005ba69540903",
+    "url": "/moviedb_frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e3935dbe5e6ad1a352c1",
+    "url": "/moviedb_frontend/static/js/2.82bfdc06.chunk.js"
+  },
+  {
+    "revision": "325fcfcf45efd286797a695777f492de",
+    "url": "/moviedb_frontend/static/js/2.82bfdc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4d727005ba69540903",
+    "url": "/moviedb_frontend/static/js/main.4b6cecd8.chunk.js"
+  },
+  {
+    "revision": "d7c736036cb5a423beb6",
+    "url": "/moviedb_frontend/static/js/runtime-main.08f4f6ad.js"
+  }
+]);
